022e67cdabfc7cba3007e6f07a80e21342ffc036
SlayZ121 <slayz9168@gmail.com>
2024-06-03 14:31:42 +0530

    yes

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from medico import app
 
-
-
 if __name__ == '__main__':
     # Delete existing items
     
--- medico/routes.py
+++ medico/routes.py
@@ -6,12 +6,45 @@ from flask import render_template, redirect, url_for, flash, request
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('./templates/home.html')
+    return render_template('home.html')
 
-@app.route('/about')
-def about_page():
-    return render_template('./templates/about.html')
 
-@app.route('/contact')
-def contact_page():
-    return render_template('./templates/contact.html')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup_page():
+    # form = RegisterForm()
+    # if form.validate_on_submit():
+    #     user_to_create = User(username=form.username.data,
+    #                           email_address=form.email_address.data,
+    #                           password=form.password1.data)
+    #     db.session.add(user_to_create)
+    #     db.session.commit()
+    #     login_user(user_to_create)
+    #     flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
+    #     return redirect(url_for('market_page'))
+    # if form.errors != {}: #If there are not errors from the validations
+    #     for err_msg in form.errors.values():
+    #         flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    # form = LoginForm()
+    # if form.validate_on_submit():
+    #     attempted_user = User.query.filter_by(username=form.username.data).first()
+    #     if attempted_user and attempted_user.check_password_correction(
+    #             attempted_password=form.password.data
+    #     ):
+    #         login_user(attempted_user)
+    #         flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+    #         return redirect(url_for('market_page'))
+    #     else:
+    #         flash('Username and password are not match! Please try again', category='danger')
+
+    return render_template('login.html', )
+
+# @app.route('/logout')
+# def logout_page():
+#     logout_user()
+#     flash("You have been logged out!", category='info')
+#     return redirect(url_for("home_page"))
