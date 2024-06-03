542517f648aaa11f12c2d1cdf60d6d0e33e88686
SlayZ121 <slayz9168@gmail.com>
2024-06-03 12:00:28 +0530

    y

--- medico/routes.py
+++ medico/routes.py
@@ -6,4 +6,12 @@ from flask import render_template, redirect, url_for, flash, request
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
+    return render_template('./templates/home.html')
+
+@app.route('/about')
+def about_page():
+    return render_template('./templates/about.html')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('./templates/contact.html')
