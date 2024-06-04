92f621a9a28013ac7ba1ff568a2b394844352057
SlayZ121 <slayz9168@gmail.com>
2024-06-04 23:43:32 +0530

    yes

--- medico/routes.py
+++ medico/routes.py
@@ -1,5 +1,30 @@
 from medico import app
 from flask import render_template, redirect, url_for, flash, request
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from random import randint
+from .mailkey import mailkey
+
+
+otp = randint(1000, 9999)
+
+def send_otp(email):
+    sender_email = 'slayz9168@gmail.com'
+    receiver_email = email
+    password = mailkey
+
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = 'Your OTP'
+
+    message.attach(MIMEText(f'Your OTP is: {otp}', 'plain'))
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
 
 
 
@@ -8,24 +33,12 @@ from flask import render_template, redirect, url_for, flash, request
 def home_page():
     return render_template('home.html')
 
+@app.route('/otp')
+def otp_page():
+    return render_template('otp.html')
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup_page():
-    # form = RegisterForm()
-    # if form.validate_on_submit():
-    #     user_to_create = User(username=form.username.data,
-    #                           email_address=form.email_address.data,
-    #                           password=form.password1.data)
-    #     db.session.add(user_to_create)
-    #     db.session.commit()
-    #     login_user(user_to_create)
-    #     flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
-    #     return redirect(url_for('market_page'))
-    # if form.errors != {}: #If there are not errors from the validations
-    #     for err_msg in form.errors.values():
-    #         flash(f'There was an error with creating a user: {err_msg}', category='danger')
 
-    return render_template('signup.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
@@ -48,3 +61,29 @@ def login_page():
 #     logout_user()
 #     flash("You have been logged out!", category='info')
 #     return redirect(url_for("home_page"))
+
+@app.route('/verify', methods=["POST"])
+def verify():
+    email = request.form['email']
+    send_otp(email)
+    return render_template('signup.html')
+
+@app.route('/validate', methods=['POST'])
+def validate():
+    user_otp = request.form['otp']
+    if otp == int(user_otp):
+        # form = RegisterForm()
+    # if request.form.validate_on_submit():
+    #     user_to_create = User(username=form.username.data,
+    #                           email_address=form.email_address.data,
+    #                           password=form.password1.data)
+    #     db.session.add(user_to_create)
+    #     db.session.commit()
+    #     login_user(user_to_create)
+    #     flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
+    #     return redirect(url_for('market_page'))
+    # if form.errors != {}: #If there are not errors from the validations
+    #     for err_msg in form.errors.values():
+    #         flash(f'There was an error with creating a user: {err_msg}', category='danger')
+        return redirect(url_for("home_page"))
+    return render_template('otp.html')
